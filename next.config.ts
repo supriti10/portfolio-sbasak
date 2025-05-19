a15e09f9fe080c7dc7d563572d1fa2cf0c771701
Supriti Basak <basaksupriti65@gmail.com>
2025-05-19 08:48:48 +0530

    Updated assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/portfolio-sbasak',  
+  assetPrefix: '/portfolio-sbasak/',
 
   eslint: {
     ignoreDuringBuilds: true,
