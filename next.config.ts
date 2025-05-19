c030e1a09bd360d6be4a157636dff45328c964b0
Supriti Basak <basaksupriti65@gmail.com>
2025-05-19 08:53:40 +0530

    Fixed scripts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/portfolio-sbasak',  
-  assetPrefix: '/portfolio-sbasak/',
 
   eslint: {
     ignoreDuringBuilds: true,
