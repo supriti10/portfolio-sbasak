2de4be3b1a08dae194c46f19222cf97d52ea088c
Supriti Basak <basaksupriti65@gmail.com>
2025-05-19 08:30:01 +0530

    Configured next.config.ts for static export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/portfolio-sbasak',  
+
   eslint: {
-    // This will allow the build to succeed even if there are ESLint errors
     ignoreDuringBuilds: true,
   },
 };
